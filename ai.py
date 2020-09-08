98475f0427dd3dd5e12d18b59e1df5fef8a5c45f
Overconfidence <53576052+Overconfidence@users.noreply.github.com>
2020-09-08 11:45:13 +0200

    Using embedding layer+classifier on IMDB data

--- ai.py
+++ ai.py
@@ -2,6 +2,8 @@ from keras.preprocessing.text import Tokenizer
 from keras.layers import Embedding
 from keras.datasets import imdb
 from keras import preprocessing
+from keras.models import Sequential
+from keras.layers import Flatten, Dense
 import numpy as np
 
 samples = ['The cat sat on the mat.', 'The dog ate my homework']
@@ -28,3 +30,11 @@ maxlen = 20
 (x_train, y_train), (x_test, y_test) = imdb.load_data(num_words=max_features)
 x_train = preprocessing.sequence.pad_sequences(x_train, maxlen=maxlen)
 x_test = preprocessing.sequence.pad_sequences(x_test, maxlen=maxlen)
+
+model = Sequential()
+model.add(Embedding(10000, 8, input_length=maxlen))
+model.add(Flatten())
+model.add(Dense(1, activation='sigmoid'))
+model.compile(optimizer='rmsprop', loss='binary_crossentropy', metrics=['acc'])
+model.summary()
+history = model.fit(x_train, y_train, epochs=10, batch_size=32, validation_split=0.2)
