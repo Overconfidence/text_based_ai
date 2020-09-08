17e8d91e6777dc1b43b30740f99c4087687d7e5a
Overconfidence <53576052+Overconfidence@users.noreply.github.com>
2020-09-08 11:40:20 +0200

    loading IMDB data

--- ai.py
+++ ai.py
@@ -1,4 +1,7 @@
 from keras.preprocessing.text import Tokenizer
+from keras.layers import Embedding
+from keras.datasets import imdb
+from keras import preprocessing
 import numpy as np
 
 samples = ['The cat sat on the mat.', 'The dog ate my homework']
@@ -17,3 +20,11 @@ for i, sample in enumerate(samples):
     for j, word in list(enumerate(sample.split()))[:max_length]:
         index = abs(hash(word) % dimensionality)
         results[i, j, index] = 1
+
+embedding_layer = Embedding(1000, 64)
+
+max_features = 10000
+maxlen = 20
+(x_train, y_train), (x_test, y_test) = imdb.load_data(num_words=max_features)
+x_train = preprocessing.sequence.pad_sequences(x_train, maxlen=maxlen)
+x_test = preprocessing.sequence.pad_sequences(x_test, maxlen=maxlen)
