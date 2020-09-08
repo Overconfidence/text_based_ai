5258f518628c36605d39e7b7fc07de11f2a2f32b
Overconfidence <53576052+Overconfidence@users.noreply.github.com>
2020-09-08 11:14:20 +0200

    hash-coding

--- ai.py
+++ ai.py
@@ -1,4 +1,5 @@
 from keras.preprocessing.text import Tokenizer
+import numpy as np
 
 samples = ['The cat sat on the mat.', 'The dog ate my homework']
 tokenizer = Tokenizer(num_words=1000)
@@ -7,3 +8,12 @@ sequences = tokenizer.texts_to_sequences(samples)
 one_hot_results = tokenizer.texts_to_matrix(samples, mode='binary')
 word_index=tokenizer.word_index
 print('Found %s unique tokens.' % len(word_index))
+
+dimensionality = 1000
+max_length = 10
+
+results = np.zeros((len(samples),max_length,dimensionality))
+for i, sample in enumerate(samples):
+    for j, word in list(enumerate(sample.split()))[:max_length]:
+        index = abs(hash(word) % dimensionality)
+        results[i, j, index] = 1
