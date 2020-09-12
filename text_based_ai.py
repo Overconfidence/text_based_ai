8bb6f237fa7c9163976d4cdb3e848778ef386df4
Overconfidence <53576052+Overconfidence@users.noreply.github.com>
2020-09-12 14:15:04 +0200

    Added new lines

--- text_based_ai.py
+++ text_based_ai.py
@@ -1,7 +1,10 @@
 from keras.preprocessing.text import Tokenizer
 from keras.preprocessing.sequence import pad_sequences
+from keras.models import Sequential
+from keras.layers import Embedding, Flatten, Dense
 import numpy as np
 import os
+import matplotlib.pyplot as plt
 
 imdb_dir = 'E:\Downloads\imdb'  # http://mng.bz/0tIo
 train_dir = os.path.join(imdb_dir, 'train')
@@ -22,7 +25,7 @@ for label_type in ['neg', 'pos']:
 
 # Listing 6.9 Tokenizing the text of the raw IMDB data
 maxlen = 100
-training_samples = 200
+training_samples = 2000
 validation_samples = 10000
 max_words = 10000
 
@@ -48,3 +51,113 @@ y_train = labels[:training_samples]
 x_val = data[training_samples:training_samples+validation_samples]
 y_val = labels[training_samples:training_samples+validation_samples]
 # end of Listing 6.9 Tokenizing the text of the raw IMDB data
+
+# Listing 6.10 Parsing the GloVe word-embeddings file
+glove_dir = 'E:\Downloads\glove'
+embedding_index = {}
+f = open(os.path.join(glove_dir, 'glove.6B.100d.txt'), encoding="utf8")
+for line in f:
+    values = line.split()
+    word = values[0]
+    coefs = np.asarray(values[1:], dtype='float32')
+    embedding_index[word] = coefs
+f.close()
+print('Found %s word vectors.' % len(embedding_index))
+# end of Listing 6.10 Parsing the GloVe word-embeddings file
+
+# Listing 6.11 Preparing the GloVe word-embeddings matrix
+embedding_dim = 100
+
+embedding_matrix = np.zeros((max_words, embedding_dim))
+for word, i in word_index.items():
+    if i < max_words:
+        embedding_vector = embedding_index.get(word)
+        if embedding_vector is not None:
+            embedding_matrix[i] = embedding_vector
+# end of Listing 6.11 Preparing the GloVe word-embeddings matrix
+
+# Listing 6.12 Model definition
+'''
+model = Sequential()
+model.add(Embedding(max_words, embedding_dim, input_length=maxlen))
+model.add(Flatten())
+model.add(Dense(32, activation='relu'))
+model.add(Dense(1, activation='sigmoid'))
+model.summary()
+'''
+# end of Listing 6.12 Model definition
+
+# Listing 6.13 Loading pretrained word embeddings into the Embedding layer
+'''
+model.layers[0].set_weights([embedding_matrix])
+model.layers[0].trainable = False
+'''
+# end of Listing 6.13 Loading pretrained word embeddings into the Embedding layer
+
+# Listing 6.14 Training and evaluation
+'''
+model.compile(optimizer='rmsprop', loss='binary_crossentropy', metrics=['acc'])
+history = model.fit(x_train, y_train, epochs=10, batch_size=32, validation_data=(x_val, y_val))
+model.save_weights('pre_trained_glove_model.h5')
+'''
+# end of Listing 6.14 Training and evaluation
+
+# Listing 6.16 Training the same model without pretrained word embeddings
+model = Sequential()
+model.add(Embedding(max_words, embedding_dim, input_length=maxlen))
+model.add(Flatten())
+model.add(Dense(32, activation='relu'))
+model.add(Dense(1, activation='sigmoid'))
+model.summary()
+
+model.compile(optimizer='rmsprop', loss='binary_crossentropy', metrics=['acc'])
+history = model.fit(x_train, y_train, epochs=10, batch_size=32, validation_data=(x_val, y_val))
+
+# end of Listing 6.16 Training the same model without pretrained word embeddings
+
+# Listing 6.15 Plotting the results
+acc = history.history['acc']
+val_acc = history.history['val_acc']
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+
+epochs = range(1, len(acc) + 1)
+
+plt.plot(epochs, acc, 'bo', label='Training acc')
+plt.plot(epochs, val_acc, 'b', label='Validation acc')
+plt.title('Training and validation accuracy')
+plt.legend()
+plt.figure()
+plt.plot(epochs, loss, 'bo', label='Training loss')
+plt.plot(epochs, val_loss, 'b', label='Validation loss')
+plt.title('Training and validation accuracy')
+plt.legend()
+plt.show()
+# end of Listing 6.15 Plotting the results
+
+# Listing 6.17 Tokenizing the data of the test set
+test_dir = os.path.join(imdb_dir, 'test')
+
+labels = []
+texts = []
+
+for label_type in ['neg', 'pos']:
+    dir_name = os.path.join(test_dir, label_type)
+    for fname in sorted(os.listdir(dir_name)):
+        if fname[-4:] == '.txt':
+            f = open(os.path.join(dir_name, fname), encoding="utf8")
+            texts.append(f.read())
+            f.close()
+            if label_type == 'neg':
+                labels.append(0)
+            else:
+                labels.append(1)
+sequences = tokenizer.texts_to_sequences(texts)
+x_test = pad_sequences(sequences, maxlen=maxlen)
+y_test = np.asarray(labels)
+# end of Listing 6.17 Tokenizing the data of the test set
+
+# Listing 6.18 Evaluating the model on the test set
+model.load_weights('pre_trained_glove_model.h5')
+model.evaluate(x_test, y_test)
+# end of Listing 6.18 Evaluating the model on the test set
