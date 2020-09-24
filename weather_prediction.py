622fa135ddce15035b8e81e91d82e9f22d2a9291
Overconfidence <53576052+Overconfidence@users.noreply.github.com>
2020-09-24 14:38:59 +0200

    Added new lines

---
+++ weather_prediction.py
@@ -0,0 +1,111 @@
+from matplotlib import pyplot as plt
+from keras.models import Sequential
+from keras import layers
+from keras.optimizers import RMSprop
+import os
+import numpy as np
+
+# Listing 6.28 Inspecting the data of the Jena weather dataset
+data_dir = 'E:/Downloads/jena_climate'
+fname = os.path.join(data_dir, 'jena_climate_2009_2016.csv')
+f = open(fname)
+data = f.read()
+f.close()
+
+lines = data.split('\n')
+header = lines[0].split(',')
+lines = lines[1:]
+
+print(header)
+print(len(lines))
+# end of Listing 6.28 Inspecting the data of the Jena weather dataset
+
+# Listing 6.29 Parsing the data
+float_data = np.zeros((len(lines), len(header) - 1))
+for i, line in enumerate(lines):
+    values = [float(x) for x in line.split(',')[1:]]
+    float_data[i, :] = values
+# end of Listing 6.29 Parsing the data
+
+#Listing 6.30 Plotting the temperature timeseries
+'''
+temp = float_data[:, 1] <1> temperature (in degrees Celsius)
+plt.plot(range(len(temp)), temp)
+plt.plot(range(1440), temp[:1440])
+'''
+# end of Listing 6.30 Plotting the temperature timeseries
+
+# Listing 6.32 Normalizing the data
+mean = float_data[:200000].mean(axis=0)
+float_data -= mean
+std = float_data[:200000].std(axis=0)
+float_data /= std
+# end of Listing 6.32 Normalizing the data
+
+# Listing 6.33 Generator yielding timeseries samples and their targets
+def generator(data, lookback, delay, min_index, max_index, shuffle=False, batch_size=128, step=6):
+    if max_index is None:
+        max_index = len(data) - delay -1
+    i = min_index + lookback
+    while i:
+        if shuffle:
+            rows = np.random.randit(min_index + lookback, max_index, size=batch_size)
+        else:
+            if i + batch_size >=max_index:
+                i = min_index + lookback
+            rows = np.arange(i, min(i + batch_size, max_index))
+            i += len(rows)
+
+        samples = np.zeros((len(rows), lookback // step, data.shape[-1]))
+        targets = np.zeros((len(rows),))
+        for j, row in enumerate(rows):
+            indices = range(rows[j] - lookback, rows[j], step)
+            samples[j] = data[indices]
+            targets[j] = data[rows[j] + delay][1]
+        yield samples, targets
+
+
+# end of Listing 6.33 Generator yielding timeseries samples and their targets
+
+# Listing 6.34 Preparing the training, validation, and test generators
+lookback = 1440
+step = 6
+delay = 144
+batch_size = 128
+
+train_gen = generator(float_data, lookback=lookback, delay=delay, min_index=0, max_index=200000,
+                      shuffle=True, step=step, batch_size=batch_size)
+val_gen = generator(float_data, lookback=lookback, delay=delay, min_index=200001, max_index=300000,
+                    step=step, batch_size=batch_size)
+test_gen = generator(float_data, lookback=lookback, delay=delay, min_index=300001, max_index=None,
+                     step=step, batch_size=batch_size)
+val_steps = (300000 - 200001 - lookback)
+test_steps = (len(float_data) - 300001 - lookback)
+# end of Listing 6.34 Preparing the training, validation, and test generators
+
+# Listing 6.35 Computing the common-sense baseline MAE
+
+
+def evaluate_naive_method():
+    batch_maes = []
+    for step in range(val_steps):
+        samples, targets = next(val_gen)
+        preds = samples[:, -1, 1]
+        mae = np.mean(np.abs(preds-targets))
+        batch_maes.append(mae)
+    print(np.mean(batch_maes))
+
+
+# evaluate_naive_method() # this line will generate an error
+celsius_mae = 0.29 * std[1]
+# end of Listing 6.35 Computing the common-sense baseline MAE
+
+# Listing 6.37 Training and evaluating a densely connected model
+model = Sequential()
+model.add(layers.Flatten(input_shape=(lookback // step, float_data.shape[-1])))
+model.add(layers.Dense(32, activation='relu'))
+model.add(layers.Dense(1))
+model.compile(optimizer=RMSprop(), loss='mae')
+history = model.fit_generator(train_gen, steps_per_epoch=500, epochs=20, validation_data=val_gen,
+                              validation_steps=val_steps)
+# end of Listing 6.37 Training and evaluating a densely connected model
