564829f63497e7e68b03846a9bd740971703bc6a
Overconfidence <53576052+Overconfidence@users.noreply.github.com>
2020-09-26 17:43:44 +0200

    Added new lines, and created new files for a different ai method

--- weather_prediction.py
+++ weather_prediction.py
@@ -2,6 +2,8 @@ from matplotlib import pyplot as plt
 from keras.models import Sequential
 from keras import layers
 from keras.optimizers import RMSprop
+from keras import imdb
+from keras.preprocessing import sequence
 import os
 import numpy as np
 
@@ -62,7 +64,7 @@ def generator(data, lookback, delay, min_index, max_index, shuffle=False, batch_
             indices = range(rows[j] - lookback, rows[j], step)
             samples[j] = data[indices]
             targets[j] = data[rows[j] + delay][1]
-        yield samples, targets
+        yield samples[:, ::-1, :], targets
 
 
 # end of Listing 6.33 Generator yielding timeseries samples and their targets
@@ -101,6 +103,7 @@ celsius_mae = 0.29 * std[1]
 # end of Listing 6.35 Computing the common-sense baseline MAE
 
 # Listing 6.37 Training and evaluating a densely connected model
+'''
 model = Sequential()
 model.add(layers.Flatten(input_shape=(lookback // step, float_data.shape[-1])))
 model.add(layers.Dense(32, activation='relu'))
@@ -108,4 +111,71 @@ model.add(layers.Dense(1))
 model.compile(optimizer=RMSprop(), loss='mae')
 history = model.fit_generator(train_gen, steps_per_epoch=500, epochs=20, validation_data=val_gen,
                               validation_steps=val_steps)
+'''
 # end of Listing 6.37 Training and evaluating a densely connected model
+
+# Listing 6.39 Training and evaluating a GRU-based model
+model = Sequential()
+model.add(layers.GRU(32, dropout=0.1, recurrent_dropout=0.5,
+                     return_sequences=True, input_shape=(None, float_data.shape[-1])))
+model.add(layers.GRU(64, activation='relu', dropout=0.1, recurrent_dropout=0.5))
+model.add(layers.Dense(1))
+model.compile(optimizer=RMSprop(), loss='mae')
+history = model.fit_generator(train_gen, steps_per_epoch=500, epochs=1, validation_data=val_gen,
+                              validation_steps=val_steps)
+# end of Listing 6.39 Training and evaluating a GRU-based model
+
+# Listing 6.38 Plotting results
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+epochs = range(1, len(loss)+1)
+plt.figure()
+plt.plot(epochs, loss, 'bo', label='Training loss')
+plt.plot(epochs, val_loss, 'b', label='Validation loss')
+plt.title('Training and validation loss')
+plt.legend()
+plt.show()
+# end of Listing 6.38 Plotting results
+
+
+# Listing 6.42 Training and evaluating an LSTM using reversed sequences
+max_features = 10000
+maxlen = 500
+
+(x_train, y_train), (x_test, y_test) = imdb.load_data(num_words=max_features)
+
+x_train = [x[::-1] for x in x_train]
+x_test = [x[::-1] for x in x_test]
+
+x_train = sequence.pad_sequences(x_train, maxlen=maxlen)
+x_test = sequence.pad_sequences(x_test, maxlen=maxlen)
+'''
+model = Sequential()
+model.add(layers.Embedding(max_features, 128))
+model.add(layers.LSTM(32))
+model.add(layers.Dense(1, activation='sigmoid'))
+model.compile(optimizer='rmsprop', loss='binary_crossentropy', metrics=['acc'])
+history = model.fit(x_train, y_train, epochs=10, batch_size=128, validation_split=0.2)
+'''
+# end of Listing 6.42 Training and evaluating an LSTM using reversed sequences
+
+# Listing 6.43 Training and evaluating a bidirectional LSTM
+'''
+model = Sequential()
+model.add(layers.Embedding(max_features, 32))
+model.add(layers.Bidirectional(layers.LSTM(32)))
+model.add(layers.Dense(1, activation='sigmoid'))
+model.compile(optimizer='rmsprop', loss='binary_crossentropy', metrics=['acc'])
+history = model.fit(x_train, y_train, epochs=10, batch_size=128, validation_split=0.2)
+'''
+#end of Listing 6.43 Training and evaluating a bidirectional LSTM
+
+# Listing 6.44 Training a bidirectional GRU
+model = Sequential()
+model.add(layers.Bidirectional(layers.GRU(32), input_shape=(None, float_data.shape[-1])))
+model.add(layers.Dense(1))
+model.compile(optimizer=RMSprop(), loss='mae')
+history = model.fit_generator(train_gen, steps_per_epoch=500, epochs=10, validation_data=val_gen,
+                              validation_steps=val_steps)
+
+# end of Listing 6.44 Training a bidirectional GRU
